90b5de1c2948309026cc693283175122b9bf27ba
trendsetter37 <javissullivan@gmail.com>
2013-11-11 21:20:14 -0500

    no errors

--- src/mit/assignment3/bestperformer/BestPerformer.java
+++ src/mit/assignment3/bestperformer/BestPerformer.java
@@ -3,24 +3,51 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package mit.assignment3.bestperformer;
 
 
 /*A group of friends perform in the Marathon
-find the best performeer and then find the second best performer
-*/
-
+ find the best performeer and then find the second best performer
+ */
 /**
  *
  * @author javis
  */
 public class BestPerformer {
-    int minTime = Integer.MAX_VALUE;
-    int minIndex = -1;
-    
-    public void bestPerformer (int mnInd, int mnTime) {
-     \}
+
+    public static int bestPerformer(int[] values) {
+        int minValue = Integer.MAX_VALUE;
+        int minIndex = -1;
+
+        for (int i = 0; i < values.length; i++) {
+            if (values[i] < minValue) {
+                minValue = values[i];
+                minIndex = i;
+            }
+            //return minValue;
+
+        }
+        return minValue;
+    }
+
+    public static int getSecondBestPerformer(int[] values) {
+        int min2Idx = -1;
+        int bestValue = bestPerformer(values);
+        int min2Value = Integer.MAX_VALUE;
+
+        for (int i = 0; i < values.length; i++) {
+            if (values[i] == bestValue) {
+                continue;
+            }
+            if (values[i] < min2Value) {
+                min2Value = values[i];
+                min2Idx = i;
+
+            }
+
+        }
+        return min2Value;
+    }
 
     /**
      * @param args the command line arguments
@@ -28,5 +55,5 @@ public class BestPerformer {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+
 }
