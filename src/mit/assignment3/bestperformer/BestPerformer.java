599499d1b7e8285f8b8468abc27d8ed4c8328285
trendsetter37 <javissullivan@gmail.com>
2013-11-11 20:02:42 -0500

    initial upload of assignment 3

---
+++ src/mit/assignment3/bestperformer/BestPerformer.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mit.assignment3.bestperformer;
+
+
+/*A group of friends perform in the Marathon
+find the best performeer and then find the second best performer
+*/
+
+/**
+ *
+ * @author javis
+ */
+public class BestPerformer {
+    int minTime = Integer.MAX_VALUE;
+    int minIndex = -1;
+    
+    public void bestPerformer (int mnInd, int mnTime) {
+     \}
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
